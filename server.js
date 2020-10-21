b487248651756697bac542384335ec60fe6c9d89
zophrox <zophrox@gmail.com>
2020-10-21 11:53:39 +0300

    added server.js file for initiall heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/browser/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
+});
+
+const port = process.env.PORT || 4200;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, '0.0.0.0', () => console.log('running dev server LOCALLY on PORT ' + port +""));
+// server.listen(port, () => console.log('running dev server on PORT ' + port +""));
